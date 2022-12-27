05a0ebbc7980c9665c58715f877b6dbd1f92c04b
Lars Peyer <l.peyer@netfire.ch>
2022-12-27 20:04:36 +0100

    Fix translation imports for django 4

--- suit/menu.py
+++ suit/menu.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 try:
     from django.utils.translation import gettext_lazy as _
 except ImportError:
-    from django.utils.translation import ugettext_lazy as _
+    from django.utils.translation import gettext_lazy as _
 
 
 class ChildItem(object):
